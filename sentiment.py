0e6215410a3376bad9c7896161d918052455193a
hanlin chen <hanlin487@gmail.com>
2025-01-30 19:41:18 -0800

    new sentiment anaylsis method

--- sentiment.py
+++ sentiment.py
@@ -1,12 +1,20 @@
 from textblob import TextBlob
 from transformers import pipeline
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-def analyze(tweet):
+def pipe_analyze(tweet):
     pipe = pipeline("sentiment-analysis")
     print(pipe(tweet))
     
+def vader_analyze(tweet):
+    nltk.download("vader_lexicon")
+    sia = SentimentIntensityAnalyzer()
+    print(sia.polarity_scores(tweet))
+
 txt = """
 “I think that it’s a great company. I do think that there’s a lot of people who believe that they will not be able to deliver on this quarter. I, therefore, am reluctant to get in ahead of the quarter and we did sell the stock a little bit higher for the charitable trust”
 """
 
-analyze(txt)
+# pipe_analyze(txt)
+vader_analyze(txt)
