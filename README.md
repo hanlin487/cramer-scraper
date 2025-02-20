1efb46d32182b33957b8560356e70087b6847081
hanlin chen <hanlin487@gmail.com>
2025-02-19 16:33:07 -0800

    wahtever the fuck this is

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 WOrk in progress for scraping Jim Cramer's tweets, analyzing the sentiment for them, highlighting if any company name is mentioned, and then displaying whether he is bullish or bearish on those companies.
 
-The point is that whatever company Jim Cramer is bullish on they tend to fall and whatever he's bearish on they tend to moon.
+whatever company Jim Cramer is bullish on they tend to fall and whatever he's bearish on they tend to moon.
--- sentiment.py
+++ sentiment.py
@@ -2,19 +2,66 @@ from textblob import TextBlob
 from transformers import pipeline
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-
-def pipe_analyze(tweet):
-    pipe = pipeline("sentiment-analysis")
-    print(pipe(tweet))
     
+def pipeline_analyze_regular(tweet):
+    sentiment_pipeline = pipeline("sentiment-analysis")
+    return sentiment_pipeline(tweet)
+
+def pipeline_analyze_financial(tweet):
+    sentiment_pipeline = pipeline(model="ahmedrachid/FinancialBERT-Sentiment-Analysis")
+    return sentiment_pipeline(tweet)
+
+def blob_analyze(tweet):
+    blob = TextBlob(tweet)
+    if -0.2 < blob.sentiment.polarity < 0.2:
+        return "neutral"
+    elif blob.sentiment.polarity > 0.2:
+        return "positive"
+    else:
+        return "negative"
+
 def vader_analyze(tweet):
-    nltk.download("vader_lexicon")
     sia = SentimentIntensityAnalyzer()
-    print(sia.polarity_scores(tweet))
+    scores = sia.polarity_scores(tweet)
+    overall = scores['compound']
+    # print(scores)
+
+    if overall < -0.05:
+        return "negative"
+    elif overall > 0.05:
+        return "positive"
+    else:
+        return "neutral"
+    
+def ngram_split(tweet):
+    blob = TextBlob(tweet)
+    return blob.ngrams(n=3)
+
+# nltk.download("vader_lexicon")
 
 txt = """
-“I think that it’s a great company. I do think that there’s a lot of people who believe that they will not be able to deliver on this quarter. I, therefore, am reluctant to get in ahead of the quarter and we did sell the stock a little bit higher for the charitable trust”
+The tariffs are light, the introduction of them heavy
 """
 
-# pipe_analyze(txt)
-vader_analyze(txt)
+grams = ngram_split(txt)
+for gram in grams:
+    data = " ".join(gram)
+    blob_result = blob_analyze(data)
+    vader_result = vader_analyze(data)
+    hf_result1 = pipeline_analyze_regular(data)
+    hf_result2 = pipeline_analyze_financial(data)
+
+    print(f'\nstring : {data}\nblob : {blob_result}\nvader: {vader_result}\nhugging face base : {hf_result1}\nhugging face financial : {hf_result2}')
+
+# while 1:
+#     user_input = input("\nEnter input: ")
+#     if user_input == "exit":
+#         break
+    
+#     try:
+#         pipe_result = 
+#         vader_result = vader_analyze(user_input)
+#         print(f'this tweet is {result} according to VADER')
+#     except Exception as err:
+#         print(f'Error in input {err}')
+#         break
