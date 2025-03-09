b84aadb31acae07ddcc1ef72812f73b767975087
hanlin chen <hanlin487@gmail.com>
2025-03-09 16:21:43 -0700

    progress ave

--- pyscripts/main.py
+++ pyscripts/main.py
@@ -95,7 +95,7 @@ def scrape_user(num_of_tweets) -> pd.DataFrame:
 
 if __name__ == "__main__":    
     # if limit rated comment these 2 lines out
-    tweets = scrape_user(10)
+    tweets = scrape_user(5)
     # storage.dataframe_to_csv(tweets)
     
     # tweets = pd.read_csv("../storage/tweets_from_scraping.csv").fillna("")
